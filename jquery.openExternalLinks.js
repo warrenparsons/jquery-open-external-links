51e046f3df42bfd845cf189112c5563310e8a924
Warren Parsons <wparsons@gmail.com>
2012-04-27 16:48:10 -0400

    Added comments and sample usage

--- jquery.openExternalLinks.js
+++ jquery.openExternalLinks.js
@@ -1,21 +1,33 @@
-/** jQuery openExternalLinks Plugin
+/**
+*	jQuery openExternalLinks Plugin
+*
+*	Plugin to open links with fully-qualified URLs in a new browser window
+*
+*	- Accepts one optional argument, which is a jQuery selector for elements that should be ignored
+*
+*	Sample usage:
+*	$('body').openExternalLinks('.colorbox');
+*
+*	This will load all fully-qualified URLs except those with a class of "colorbox"
+*	in a new browser window.
 *
 *	Created by Warren L. Parsons
 *	Copyright 2012
-*
 */
 
-
 (function($){
-	$.fn.setExternalLinks = function(exceptions){
+	$.fn.openExternalLinks = function(exceptions){
 
 		$(this).on('click', 'a[href^="http"]', function(e){
 
+			// If this link matches the list of exceptions, return
 			if ( $(this).is(exceptions) ) {
 				return $(this);
 			}
 
 			e.preventDefault();
+
+			// Open link in new browser window
 			window.open(this.href);
 
 		});
